4f874f1416202394f2f316a4280536b5550572d9
Jason Mannon <jmannonpdx@gmail.com>
2014-06-10 11:23:54 -0700

    buttons is functioning

--- src/com/example/crystalball/MainActivity.java
+++ src/com/example/crystalball/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.crystalball;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -17,6 +14,21 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Declare view variables and assign them Views from the layouts file
+        final TextView answerLabel = (TextView) findViewById(R.id.textView1);
+        Button getAnswerButton = (Button) findViewById(R.id.button1);
+        
+        getAnswerButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// The button was clicked, so update the answer label with an answer
+				String answer = "Yes";
+				answerLabel.setText(answer);
+				
+			}
+		});
     }
 
 
@@ -26,6 +38,7 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+        
     }
 
     @Override
