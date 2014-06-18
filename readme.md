8ddd6cbd47db405e1410b45ace6baf28525de147
Jason Mannon <jmannonpdx@gmail.com>
2014-06-18 13:24:49 -0700

    edited readme to link to google play store

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 Crystal Ball App
 =========
 
-Download the app from here: https://www.dropbox.com/s/xgcba3vef82ogcc/CrystalBall.apk
+Download the app from here: https://play.google.com/store/apps/details?id=com.jasonm89.crystalball
 
   - Ask a question and let the all mighty crystal ball take care of the rest
---
+++ src/com/jasonm89/crystalball/CrystalBall.java
@@ -0,0 +1,26 @@
+package com.jasonm89.crystalball;
+
+import java.util.Random;
+
+public class CrystalBall {
+	public String[] mAnswers = {
+			"It is certain",
+			"It is decidingly so",
+			"All signs say YES",
+			"The stars are not aligned",
+			"My reply is no",
+			"Better not tell you now",
+			"Concentrate and ask again",
+			"Unable to answer now",
+			"It is hard to say" 
+		};
+	
+	public String getAnAnswer() {
+		String answer = "";
+			
+		Random randomGenerator = new Random(); //Construct a new random number generator
+		int randomNumber = randomGenerator.nextInt(mAnswers.length);
+			answer = mAnswers[randomNumber];
+			return answer;
+	}
+}
